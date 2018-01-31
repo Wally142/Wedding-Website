24f8718310f450525b008ed278dc1be28b7998f2
Greg.Wallerus <gwallerus@gmail.com>
2018-01-31 08:33:07 -0600

    skeleton started

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,22 @@
-console.log('hello');
+var myApp = angular.module('myApp', ['ngRoute']);
+
+/// Routes ///
+myApp.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+        .when('/home', {
+            templateUrl: '/views/templates/home.html',
+            controller: 'WeddingController as wc',
+        }).when('/hotel', {
+            templateUrl: '/views/templates/hotel.html',
+            controller: 'WeddingController as wc',
+        }).when('/contact', {
+            templateUrl: '/views/templates/contact.html',
+            controller: 'WeddingController as wc',
+        }).when('/WeddingParty', {
+            templateUrl: '/views/templates/weddingParty.html',
+            controller: 'WeddingController as wc',
+        }).otherwise({
+            redirectTo: 'home'
+        });
+});
